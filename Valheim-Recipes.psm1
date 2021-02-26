66057131cffd0bed0afdeca824e7293560901c09
Kellen Stuart <kellen.m.stuart@gmail.com>
2021-02-25 18:41:58 -0700

    Made first recipe

---
+++ Valheim-Recipes.psm1
@@ -0,0 +1,34 @@
+function Get-Recipe
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name
+    )
+
+    $recipeDirectory = "$PSScriptRoot\Recipes"
+    $recipeJsonFiles = Get-ChildItem $recipeDirectory -Recurse -File
+
+    foreach($recipeJsonFile in $recipeJsonFiles)
+    {
+        $recipeName = Get-RecipeName $recipeJsonFile
+        if($recipeName.Contains($Name))
+        {
+            $recipeObject = Get-Content $recipeJsonFile.FullName | Out-String | ConvertFrom-Json
+            $recipeObject
+        }
+    }
+
+}
+
+function Get-RecipeName
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $FileName
+    )
+    return $FileName.Substring(0, $FileName.IndexOf('.'))
+}
